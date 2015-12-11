053efde1ce77648b8304059df98e01e20d87ce87
higuma <nomail@nomail.com>
2015-12-12 00:41:40 +0900

    クイックソート追加

---
+++ sort/quick_sort.py
@@ -0,0 +1,14 @@
+def quick_sort(arr):
+    if(len(arr) <= 1):
+        return arr
+
+    pivot = arr[0]
+    left = []
+    right = []
+    for i in range(1, len(arr)):
+        if(arr[i] > pivot):
+            right.append(arr[i])
+        else:
+            left.append(arr[i])
+
+    return quick_sort(left) + [pivot] + quick_sort(right)
