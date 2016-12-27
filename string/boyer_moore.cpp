1a32b14d91d0850a70c96de18d59fe3108fc4119
Higumabear <sckkt1s.system@gmail.com>
2016-12-27 10:31:16 +0900

    boyer-moore debugged

--- string/boyer_moore.cpp
+++ string/boyer_moore.cpp
@@ -7,15 +7,15 @@ using namespace std;
 vector<int> boyer_moore(string text, string pattern){
   int N = text.length(), M = pattern.length();
   vector<int> skip(0x100, M);
-  for(int i = 0; i < M; i++) skip[pattern[i]] = M - i + 1;
+  for(int i = 0; i < M - 1; i++) skip[pattern[i]] = M - i - 1;
 
   vector<int> pos;
   for(int i = M - 1; i < N; ){
     int j = M - 1;
-    while(j >= 0 && text[i] == pattern[j]) i--, j--;
+    while(j >= 0 && text[i] == pattern[j]) --i, --j;
     if(j < 0){
       pos.push_back(i + 1);
-      i += M + 1;
+      i += M + 1, j = M - 1;
     }
     else i += skip[text[i]];
   }
@@ -25,6 +25,7 @@ vector<int> boyer_moore(string text, string pattern){
 int main(){
   string t = "aaaaaaaaaa";
   string s = "aa";
+  cin >> t >> s;
   auto v = boyer_moore(t, s);
   for(int i = 0; i < v.size(); i++) cout << v[i] << " ";
   cout << endl;
