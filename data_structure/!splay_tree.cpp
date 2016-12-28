e39b4ad73a66e2a7f2ca8d6c931f9eea480b8ef7
Higumabear <sckkt1s.system@gmail.com>
2016-12-28 17:11:03 +0900

    Bottom-up Splay木 途中

--- data_structure/!splay_tree.cpp
+++ data_structure/!splay_tree.cpp
@@ -3,13 +3,13 @@
 
 using namespace std;
 
-template <typename T> class splay{
+template <typename T> class splay_tree{
   struct node{
     T val;
     node *l, *r, *par;
     node(const T &val = 0, node *par = 0, node *l = 0, node *r = 0) : val(val), l(l), r(r), par(par) {}
   } *root;
-  splay() : root(NULL) {};
+  splay_tree() : root(NULL) {};
 
   node *rrot(node *x){
     node *y = x->l;
@@ -23,5 +23,42 @@ template <typename T> class splay{
     y->l = x;
     return y;
   }
-  
+  void splay(node *x){
+    node *y;
+    if(!x->par) return;
+    if(!x->par->par){/* zig */
+      if(x->par->l == x){ y = rrot(x->par); }
+      else              { y = lrot(x->par); }
+    }else{
+      if(x->par->l == x){
+	if(x->par->par->l == x->par){ // zig-zig
+	  rrot(x->par->par);
+	  y = rrot(x->par);
+	}else{ //zig-zag
+	  rrot(x->par);
+	  y = lrot(x->par);
+	}
+      }else{
+	if(x->par->par->r == x->par){ // zig-zig
+	  lrot(x->par->par);
+	  y = lrot(x->par);
+	}else{// zig-zag
+	  lrot(x->par);
+	  y = rrot(x->par);
+	}
+      }
+    }
+    splay(y);
+  }
+  node *find(node *x, const T &key){
+    if(!x) return NULL;
+    if(x->key == key){
+      splay(x);
+      return x;
+    }
+    return (x->key > key) ? find(x->l) : find(x->r);
+  }
+  node *insert(node *x, const T &key){
+    
+  }
 };
