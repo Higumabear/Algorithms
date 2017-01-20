39225e260e4c58688ac80c959b514ded484138b1
Higumabear <sckkt1s.system@gmail.com>
2017-01-20 13:43:42 +0900

    garcia-wachs with bugs

--- data_structure/_garcia_wachs.cpp
+++ data_structure/_garcia_wachs.cpp
@@ -1,43 +1,67 @@
 #include <iostream>
+#include <cstdio>
 #include <vector>
 #include <queue>
 
 typedef long long ll;
 using namespace std;
 
-struct node{
-  node *l, *r;
-  ll w;
-  node(ll w, node *l = 0, node *r = 0) : w(w), l(l), r(r) {}
-  bool operator>(node *r) const {return w > r->w;}
-} *root;
+struct garsia_wachs{
+  struct node{
+    ll w;
+    node *l, *r;
+    node(ll w, node *l = 0, node *r = 0) : w(w), l(l), r(r) {}
+  } *root;
+  struct nodecmp{
+    bool operator()(const node *x, const node *y) const {return x->w > y->w;}
+  };
+  priority_queue<node*, vector<node*>, nodecmp> q;
 
-void garsia_wachs(int *w, int N){
-  priority_queue<node*, vector<node*>, greater<node*> > q;
-  for(int i = N - 1; i >= 0; i--){
-    if(i >= 2 && w[i - 2] >= w[i]){
-      q.push(new node(w[i - 1] + w[i], 
-		      new node(w[i - 1]), new node(w[i])));
-      i--;
-    }else q.push(new node(w[i]));
+  garsia_wachs(const vector<ll> &w) { build(w); }
+  
+  void build(const vector<ll> &w){
+    int N = w.size();
+    //q = priority_queue<node*, vector<node*>, nodecmp>();
+    for(int i = N - 1; i >= 0; i--){
+      if(i >= 2 && w[i - 2] >= w[i]){
+	q.push(new node(w[i - 1] + w[i], 
+			new node(w[i - 1]), new node(w[i])));
+	i--;
+      }
+      else q.push(new node(w[i]));
+    }
+    while(q.size() > 1){
+      node *a = q.top(); q.pop();
+      node *b = q.top(); q.pop();
+      q.push(new node(a->w + b->w, b, a));
+    }
+    root = q.top();
   }
-  while(q.size() > 1){
-    node *a = q.top(); q.pop();
-    node *b = q.top(); q.pop();
-    q.push(new node(a->w + b->w, a, b));
-  }
-  root = q.top();
-}
 
-ll find(node *p, int depth = 0){
-  if(!p) return 0;
-  if(!p->l && !p->r) return depth * p->w;
-  return find(p->l, depth + 1) + find(p->r, depth + 1);
-}
+  ll mincost(){ return mincost(root, 0); }
+  ll mincost(node *p, int depth){
+    if(!p) return 0;
+    if(!p->l && !p->r) return depth * p->w;
+    return mincost(p->l, depth + 1) + mincost(p->r, depth + 1);
+  }
+  void debug(){ dbg(root, root, 0); }
+   void dbg(node *p, node *par, int depth){
+    if(!p) return ;
+    cout << par->w << " -> " << p->w << " [ label = " << depth << " ];" << endl;
+    if(!p->l && !p->r) return;
+    dbg(p->l, p, depth + 1);
+    dbg(p->r, p, depth + 1);
+    return ;
+  }
+};
 
 int main(){
-  int w[12] = {80,12,10,11,13,3,4,5,9,8,7,25};
-  garsia_wachs(w, 12);
-  cout << root->w << endl;
-  cout << find(root, 0) << endl;
+  int n;
+  cin >> n;
+  vector<ll> weight(n);
+  for(int i = 0; i < n; i++) cin >> weight[i];
+
+  garsia_wachs gw(weight);
+  cout << gw.mincost() << endl;
+  gw.debug();
 }
