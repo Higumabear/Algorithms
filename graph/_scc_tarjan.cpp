b376bc02ac5e01c6d4a07ccf44d0719e44ff47d0
Higumabear <sckkt1s.system@gmail.com>
2017-02-23 18:51:18 +0900

    modify scc_tarjan

--- graph/_scc_tarjan.cpp
+++ graph/_scc_tarjan.cpp
@@ -11,44 +11,60 @@
 #include <vector>
 #include <map>
 #include <cstdio>
+#include <stack>
+#include <iostream>
 
 using namespace std;
+
+const int INF = 1 << 29;
 
 struct scc{
   int V, c;
   vector<vector<int> > g;
   vector<int> disc, low;
   vector<int> cmp;
-  vector<bool> vis;
-  map<pair<int, int>, bool> used;
-  scc(int V) : V(V), g(V), disc(V, -1), 
-	       low(V, -1), vis(V), cmp(V), c(0) {}
+  vector<bool> onstack;
+  stack<int> S;
+  scc(int V) : V(V), g(V), disc(V), 
+	       low(V), onstack(V), cmp(V), c(0) {}
   
   void add_edge(int from, int to){
     g[from].push_back(to);
+    //g[to].push_back(from);
   }
 
-  void dfs(int u){
-    disc[u] = c++;
-    low[u] = disc[u];
-    vis[u] = true;
-    for(int v = 0; v < g[u].size(); v++){
-      if(!vis[v]){
-	used[make_pair(u, v)] = true;
-	dfs(v);
-	low[u] = min(low[u], low[v]);
-      }else if(!used.count(make_pair(u, v))){
-	low[u] = min(low[u], disc[v]);
+  void dfs(int v){
+    low[v] = disc[v] = ++c;
+    onstack[v] = true;
+    S.push(v);
+
+    for(int i = 0; i < g[v].size(); i++){
+      int u = g[v][i];
+      if(disc[u] == 0){
+	dfs(u);
+	low[v] = min(low[v], low[u]);
+      }else if(onstack[u]){
+	low[v] = min(low[v], disc[u]);
+      }
+    }
+    if(low[v] == disc[v]){
+      while(1){
+	int w = S.top(); S.pop();
+	onstack[w] = false;
+	if(v == w) break;
       }
     }
   }
 
   int decomp(){
     for(int i = 0; i < V; i++){
-      if(vis[i]) continue;
-      dfs(i);
+      if(disc[i] == 0) dfs(i);
     }
     
+    for(int i = 0; i < V; i++){
+      cout << i << " " << disc[i] << " " << low[i] << endl;
+    }
+
     int k = 0;
     map<int, int> label;
     for(int i = 0; i < V; i++)
